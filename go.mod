73ea29acab12c35611d2bd63c032d1cf53ef2838
Tiến Lực Nguyễn <matkinhig@gmail.com>
2019-10-11 16:56:17 +0700

    golang restful GET_POST

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
-module echo-fw
+module github.com/matkinhig/echo-fw
 
 go 1.13
+
+require (
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/leekchan/accounting v0.0.0-20190702062627-a09595581342
+	github.com/mattn/go-oci8 v0.0.0-20191007070605-96a3284b269c
+	github.com/spf13/viper v1.4.0
+)
--- main.go
+++ main.go
@@ -1,11 +1,36 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 
 	"github.com/leekchan/accounting"
 	"github.com/matkinhig/echo-fw/config"
+
+	"net/http"
+
+	"github.com/labstack/echo"
+
+	"database/sql"
+
+	_ "github.com/mattn/go-oci8"
 )
+
+type User struct {
+	Name    string `json:name`
+	Address string `json:address`
+}
+
+type Student struct {
+	FirstName   string `json:"first_name"`
+	LastName    string `json:"last_name"`
+	Email       string `json:"email"`
+	Age         int    `json:"age"`
+	ClassName   string `json:"course_name"`
+	AcademyName string `json:"acedemy_name"`
+}
 
 func main() {
 	fmt.Println("start golang")
@@ -14,5 +39,111 @@ func main() {
 	fmt.Println(ac.FormatMoney(123456789.213123))
 
 	fmt.Println("The Config value:", config.Config)
+	testServer()
+	// connectORACLE()
+}
 
+func testServer() {
+	e := echo.New()
+	e.GET("/", allocServer)
+	e.GET("/info/:id", getInfomation)
+	e.POST("info", addUser)
+	e.POST("/student", addStudent)
+	e.POST("/people", addPeople)
+	e.Start(":8000")
+}
+
+func addPeople(c echo.Context) error {
+	aPeople := Student{}
+	defer c.Request().Body.Close()
+	err := c.Bind(&aPeople) //cach 3 bind Data request input
+	if err != nil {
+		log.Printf("can not decode the request : %s", err)
+		return echo.NewHTTPError(http.StatusInternalServerError)
+	}
+	log.Printf("this is a people %#v : ", aPeople)
+	return c.String(http.StatusOK, "we are receive the people %s : ")
+}
+
+func addStudent(c echo.Context) error {
+	aStudent := Student{}
+
+	defer c.Request().Body.Close()
+
+	err := json.NewDecoder(c.Request().Body).Decode(&aStudent) //cach 2 NewDecoder
+	if err != nil {
+		log.Printf("can not decode the request : %s", err)
+		return echo.NewHTTPError(http.StatusInternalServerError)
+	}
+	log.Printf("this is a student %#v : ", aStudent)
+	return c.String(http.StatusOK, "we are receive the student %s : ")
+}
+
+func allocServer(c echo.Context) error {
+	return c.String(http.StatusOK, "my first website by golang")
+}
+
+func getInfomation(c echo.Context) error {
+	name := c.QueryParam("name")
+	address := c.QueryParam("address")
+
+	dataType := c.Param("data")
+
+	if dataType == "string" {
+		return c.String(http.StatusOK, fmt.Sprintf("your name : %s \n your address : %s \n", name, address))
+	}
+	if dataType == "json" {
+		return c.JSON(http.StatusOK, map[string]string{
+			"name":    name,
+			"address": address,
+		})
+	}
+
+	return c.JSON(http.StatusBadRequest, map[string]string{
+		"error": "cant parse to json",
+	})
+}
+
+func addUser(c echo.Context) error {
+	teo := User{}
+
+	b, err := ioutil.ReadAll(c.Request().Body) //cach 1 post data
+
+	if err != nil {
+		log.Println("cant read data request %s", err)
+		return c.String(http.StatusInternalServerError, "")
+	}
+
+	err = json.Unmarshal(b, &teo)
+	if err != nil {
+		log.Println("cant unmarshal json %s", err)
+		return c.String(http.StatusInternalServerError, "")
+	}
+
+	defer c.Request().Body.Close()
+	fmt.Printf("info cua teo la : %+v", teo)
+
+	return c.String(http.StatusOK, "add user")
+}
+
+func connectORACLE() {
+	println("start")
+	db, err := sql.Open("oci8", "USER_NHS/nhsVB123456@192.168.254.34:1521/sid?tcbsweb")
+	if err != nil {
+		log.Fatal(err)
+	}
+	println("Connection succcess!!")
+	rows, err := db.Query("SELECT sysdate  FROM dual")
+	if err != nil {
+		log.Fatalln("err:", err.Error)
+	}
+	var (
+		sysdate string
+	)
+	for rows.Next() {
+		if err = rows.Scan(&sysdate); err != nil {
+			log.Fatalln("error fetching", err)
+		}
+		log.Println(sysdate)
+	}
 }
